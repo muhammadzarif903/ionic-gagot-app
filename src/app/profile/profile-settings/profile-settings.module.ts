36eddfad748392c212c305b9c274245dcdfefcd8
maximlivshitz <maxim.livshitz@locusview.com>
2022-03-07 00:32:39 +0200

    background colors

--- src/app/profile/profile-settings/profile-settings.module.ts
+++ src/app/profile/profile-settings/profile-settings.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
-import { IonicModule } from '@ionic/angular';
-import { ProfileSettingsPageRoutingModule } from './profile-settings-routing.module';
-import { ProfileSettingsPage } from './profile-settings.page';
-import { SharedModule } from '../shared/shared.module';
-import { TranslateModule } from '@ngx-translate/core';
+import {IonicModule} from '@ionic/angular';
+import {ProfileSettingsPageRoutingModule} from './profile-settings-routing.module';
+import {ProfileSettingsPage} from './profile-settings.page';
+import {TranslateModule} from '@ngx-translate/core';
+import {SharedModule} from '../../shared/shared.module';
+import {UpdatePersonalProfileComponent} from './update-personal-profile/update-personal-profile.component';
+import {SettingsComponent} from './settings/settings.component';
 
 @NgModule({
   imports: [
@@ -17,6 +19,7 @@ import { TranslateModule } from '@ngx-translate/core';
     SharedModule,
     TranslateModule
   ],
-  declarations: [ProfileSettingsPage]
+  declarations: [ProfileSettingsPage, UpdatePersonalProfileComponent, SettingsComponent]
 })
-export class ProfileSettingsPageModule {}
+export class ProfileSettingsPageModule {
+}
